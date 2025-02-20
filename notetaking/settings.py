e109c91bebd3a9474216dc15e1730c9a25e4af0e
mwamuziscode <mwamuziscode@gmail.com>
2025-02-20 17:57:21 +0000

    added to codebase

--- notetaking/settings.py
+++ notetaking/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 try:
     print(os.environ['DJANGO_KEY'])
 except KeyError:
-    print("KeyError: 'DJANGO_KEY='")
+    print("KeyError: 'DJANGO_KEY'")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY= os.environ['DJANGO_KEY']
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #'rest_framework',
     'notes',
-    'crispy_forms',
+    # 'crispy_forms',
     #'django_ckeditor_5',
 ]
 
